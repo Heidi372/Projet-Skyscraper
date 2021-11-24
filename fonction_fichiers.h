c4faf65e58a455a86783be4aebb9ccee768caaa0
Heidi372 <astridganlonon@gmail.com>
2021-11-24 12:47:53 +0100

    fonction pour la lecture d'un fichier

---
+++ fonction_fichiers.h
@@ -0,0 +1,16 @@
+#ifndef FONCTION_FICHIERS_H
+#define FONCTION_FICHIERS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <SDL2/SDL_image.h>
+
+char** allouer_tab_2D(int n,int m);
+void desallouer_tab_2D(char** tab, int n);
+void afficher_tab_2D(char** tab, int n, int m);
+void taille_fichier(const char* nomFichier, int* nbLig, int* nbCol);
+char** lire_fichier(const char* nomFichier);
+
+#endif //FONCTION_FICHIERS_H
